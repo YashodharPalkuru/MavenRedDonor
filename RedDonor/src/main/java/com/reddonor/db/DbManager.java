49c460b7177dad4308e88ad44bca3161eca413e5
YashodhP <YashodhP@HYRDLT1753.ES.AD.ADP.com>
2016-05-12 00:33:59 +0530

    Commit from JBoss Tools

--- RedDonor/src/main/java/com/reddonor/db/DbManager.java
+++ RedDonor/src/main/java/com/reddonor/db/DbManager.java
@@ -27,15 +27,15 @@ public class DbManager
     			//String url = "jdbc:mysql://sql6.freemysqlhosting.net:3306/sql6111909";
     			//String url = "jdbc:mysql://127.4.175.2:3306/reddonordb";
     			//String url = "jdbc:mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/bloodindia";
-    			String url = "jdbc:mysql://127.6.10.2:3306/bloodindia";
+    			String url = "jdbc:mysql://127.13.164.130:3306/reddonor";
 
 				//mysql://adminA1QamE9:RmkJI5gU3j9g@127.4.175.2:3306
 				
 				Class.forName("com.mysql.jdbc.Driver");
 				//String string = "sql6111909";
-				String uname = "adminphw77zK";
+				String uname = "admintdidjAg";
 				//String pwd = "HuLVhXzVTe";
-				String pwd = "u4NF32pMuvqp";
+				String pwd = "XQ87CNJYVub4";
 				connection = DriverManager.getConnection(url, uname,pwd);
 			} 
     		catch (Exception e) 
